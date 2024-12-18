1264c16e76ba0a8ba807eb0a0809a7ef80b0d04d
Milad Natanzi <miladnatanzi@gmail.com>
2024-12-17 19:44:16 -0500

    FYI

--- Data/dataset_generator/mimo_dataset_generator.py
+++ Data/dataset_generator/mimo_dataset_generator.py
@@ -471,7 +471,7 @@ class MIMODatasetGenerator:
 # Example usage
 def main():
     generator = MIMODatasetGenerator()
-    generator.generate_dataset(num_samples=100_000)
+    generator.generate_dataset(num_samples=10_000)
     generator.verify_dataset('dataset/mimo_dataset.h5')
 
 if __name__ == "__main__":
