f6c0b30277aacafbde87d6213804c6d812adf891
Milad Natanzi <miladnatanzi@gmail.com>
2024-12-17 19:46:38 -0500

    FIX

--- Data/main.py
+++ Data/main.py
@@ -29,7 +29,7 @@ def parse_arguments():
     parser.add_argument(
         '--samples', 
         type=int, 
-        default=1_000_000, 
+        default=10_000, 
         help="Number of samples to generate (default: 1,000,000)"
     )
     
